96149cd5de31f24bc31ec455ccbb82d58108bae1
ltaustinrobb <ltaustinrobb@gmail.com>
2017-10-20 13:29:53 +0000

    tidied up main file

--- sourcecode/collection.py
+++ sourcecode/collection.py
@@ -5,6 +5,25 @@ with app.open_resource('data/books.json') as book_file:
   data = json.load(book_file)
   global book_list
   book_list = data['books']
+
+def getlist(sort):
+  list=[]
+  for elem in book_list:
+    for k,v in elem.iteritems():
+      if k == sort:
+        if v not in list:
+          list.append(v)
+          list.sort()
+  return list
+
+def getsplist(sort):
+  list=[]
+  for elem in book_list:
+    for k,v in elem.iteritems():
+      if v == sort:
+        list.append(elem['title'])
+        list.sort()
+  return list
 @app.route('/',methods=['GET','POST'])
 def index():
  # global d
@@ -12,13 +31,9 @@ def index():
 
 @app.route('/title')
 def titles():
-  tlist=[]
-  for elem in book_list:
-    for k,v in elem.iteritems():
-      if k == 'title':
-        tlist.append(v)
-        tlist.sort()
-  return render_template("titles.html",list=tlist)
+  sort='title'
+  list=getlist(sort)
+  return render_template("titles.html",list=list)
 
 @app.route('/title/<title>')
 def title(title):
@@ -26,66 +41,51 @@ def title(title):
 
 @app.route('/author')
 def author():
-  global alist 
-  alist =[]
-  for elem in book_list:
-    for k,v in elem.iteritems():
-      if k == 'author':
-        if v not in alist:
-          alist.append(v)
-          alist.sort()
-  return render_template("author.html",list=alist)
+  sort= 'author'
+  list=getlist(sort)
+  return render_template("author.html",list=list)
 
 @app.route('/author/<spauthor>')
 def spauthor(spauthor):
-  return render_template("spauthor.html",author=spauthor,list=book_list,alist=alist)
-
+  sort = spauthor
+  list=getsplist(sort)
+  return render_template("spauthor.html",author=spauthor,list=list)
 @app.route('/publisher')
 def publisher():
-  global plist
-  plist = []
-  for elem in book_list:
-    for k,v in elem.iteritems():
-      if k == 'publisher':
-        if v not in plist:
-          plist.append(v)
-          plist.sort()
-  return render_template("publishers.html",list=plist)
+  sort = 'publisher'
+  list = getlist(sort)
+  return render_template("publishers.html",list=list)
 
 
 @app.route('/publisher/<sppub>')
 def sppub(sppub):
-  return render_template("sppub.html",sppub=sppub,list=book_list,plist=plist)
+  sort = sppub
+  list = getsplist(sort)
+  return render_template("sppub.html",sppub=sppub,list=list)
 
 @app.route('/genre')
 def genre():
-  glist = []
-  for elem in book_list:
-    for k,v in elem.iteritems():
-      if k =='genre':
-        if v not in glist:
-          glist.append(v)
-          glist.sort()
-  return render_template("genres.html", list=glist)
+  sort = 'genre'
+  list = getlist(sort)
+  return render_template("genres.html", list=list)
 
 @app.route('/genre/<spgenre>')
 def spgenre(spgenre):
-  return render_template("spgenre.html",spgenre=spgenre,list=book_list)
+  sort=spgenre
+  list=getsplist(sort)
+  return render_template("spgenre.html",spgenre=spgenre,list=list)
 
 @app.route('/year')
 def year():
-  ylist=[]
-  for elem in book_list:
-    for k,v in elem.iteritems():
-      if k =='published':
-        if v not in ylist:
-          ylist.append(v)
-          ylist.sort()
-  return render_template("year.html",list=ylist)
+  sort='published'
+  list=getlist(sort)
+  return render_template("year.html",list=list)
 
 @app.route('/year/<spyear>')
 def spyear(spyear):
-  return render_template("spyear.html",spyear=spyear,list=book_list)
+  sort=spyear
+  list=getsplist(sort)
+  return render_template("spyear.html",spyear=spyear,list=list)
 
 
 
